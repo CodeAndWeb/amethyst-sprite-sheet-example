1449145bf0ffd91ac031c4f895e2d14def09793d
Joachim Grill <joachim@codeandweb.com>
2019-10-02 13:23:27 +0200

    added TexturePacker sprite sheet

---
+++ src/state/sprites.rs
@@ -0,0 +1,13 @@
+//
+//  Sprite sheet data for Amethyst Game Engine: https://www.amethyst.rs
+//  This file was created by TexturePacker: https://www.codeandweb.com/texturepacker
+//
+//  $TexturePacker:SmartUpdate:952fa0653caf8665ea6c18a65f7fab1f:93556a30a7df481d81583df7522451bb:56397e2a57cf7772eb2a941aa2b309a5$
+//
+
+#![allow(dead_code)]
+
+pub const BANANA: usize = 0;
+pub const CHERRIES: usize = 1;
+pub const ORANGE: usize = 2;
+
